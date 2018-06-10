6ececb0a59db6c0ce587b9b19113ef02899548fb
Aliaksei Kliuchnikau <aliaksei.kliuchnikau@gmail.com>
2018-06-10 15:42:18 +0200

    Add CLI command "pull"

--- status-page.rb
+++ status-page.rb
@@ -1,8 +1,19 @@
 #!/usr/bin/env ruby
 
+require 'thor'
 require_relative "lib/lib"
 
-puts StatusSource::Github.new().current_status
-puts StatusSource::Bitbucket.new().current_status
-puts StatusSource::Rubygems.new().current_status
-puts StatusSource::Cloudflare.new().current_status
+class StatusPageCLI < Thor
+
+  desc "pull", "Pull all the status page data from different \
+                providers and save into the data store."
+  def pull
+    puts StatusSource::Github.new().current_status
+    puts StatusSource::Bitbucket.new().current_status
+    puts StatusSource::Rubygems.new().current_status
+    puts StatusSource::Cloudflare.new().current_status
+  end
+
+end
+
+StatusPageCLI.start(ARGV)
